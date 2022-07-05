3600ab09c1542914bffaa33c242a50f013cc4f2e
Alexey19800506 <osiyuk19800506@gmail.com>
2022-07-05 14:31:05 +0300

    t6

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export function setTitle(text) {
-    const titleElem = document.querySelector('.title');
-    titleElem.textContent = text;
+export function setButton(buttonText) {
+    const buttonElem = document.querySelector('body');
+    buttonElem.innerHTML = buttonText;
 }
