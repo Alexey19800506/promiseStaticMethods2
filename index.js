fc50ab855279c2902f6b542db776ce0316eba320
Alexey19800506 <osiyuk19800506@gmail.com>
2022-07-05 13:13:59 +0300

    t3

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-export function getTitleElement() {
-    const titleElem = document.querySelector('.title');
-    console.dir(titleElem);
-    return titleElem;
+export function getItemList() {
+    const elementsList = document.querySelectorAll('.technology');
+    console.dir(elementsList);
+    return elementsList;
 }
 
-export function getInputElement() {
-    const inputElem = document.querySelector('input');
-    console.dir(inputElem);
-    return inputElem;
+export function getInputArray() {
+    const NodeList = document.querySelectorAll('.tool');
+    const elementsArray = Array.from(NodeList);
+    console.dir(elementsArray);
+    return elementsArray;
 }
