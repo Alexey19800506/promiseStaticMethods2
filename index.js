a9b0b28e0eb089f4e1077e7f3c943c2907b5a1eb
Alexey19800506 <osiyuk19800506@gmail.com>
2022-07-06 11:47:04 +0300

    t11

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-export function finishform() {
-    const el = document.querySelector('input');
-    el.setAttribute('type', 'password')
-    const elem = document.querySelector('.login-form');
-    const createElem = document.createElement('input');
-    elem.prepend(createElem);
-    createElem.setAttribute('type', 'text');
-    createElem.setAttribute('name', 'login');
+export function manageClasses() {
+    const elem = document.querySelector('.one');
+    elem.classList.add('selected');
+    const elem1 = document.querySelector('.two');
+    elem1.classList.remove('selected');
+    const elem2 = document.querySelector('.three');
+    elem2.classList.toggle('three_done');
+    const elem3 = document.querySelector('.four');
+    if (elem3.classList.contains('some-class')) {
+        elem3.classList.add('another-class');
+    }
 }
