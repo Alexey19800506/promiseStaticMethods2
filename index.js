c47b74d3bc10c478283daeba9bd1f568712056d1
Alexey19800506 <osiyuk19800506@gmail.com>
2022-07-06 10:52:20 +0300

    t9

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-export function createButton(buttonText) {
-    const elem = document.querySelector('body');
-    const createElem = document.createElement('button');
-    elem.append(createElem);
-    createElem.textContent = buttonText;
+export function finshList() {
+    const elem = document.querySelector('.list');
+    const createElem = document.createElement('li');
+    elem.prepend(createElem);
+    createElem.textContent = '1';
+    const createElem1 = document.createElement('li');
+    elem.append(createElem1);
+    createElem1.textContent = '8';
+    const elem1 = document.querySelector('.special');
+    const createElem2 = document.createElement('li');
+    elem1.before(createElem2);
+    createElem2.textContent = '4';
+    const createElem3 = document.createElement('li');
+    elem1.before(createElem3);
+    createElem3.textContent = '6';
 }
