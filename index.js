e8b3e81b3c770f884b97163151841eae621d491a
Alexey19800506 <osiyuk19800506@gmail.com>
2022-07-06 12:57:43 +0300

    t12

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-export function manageClasses() {
-    const elem = document.querySelector('.one');
-    elem.classList.add('selected');
-    const elem1 = document.querySelector('.two');
-    elem1.classList.remove('selected');
-    const elem2 = document.querySelector('.three');
-    elem2.classList.toggle('three_done');
-    const elem3 = document.querySelector('.four');
-    if (elem3.classList.contains('some-class')) {
-        elem3.classList.add('another-class');
+export function squaredNumbers() {
+    const elements = document.querySelectorAll('.number');
+    for (let elem of elements) {
+        elem.dataset.squaredNumber = elem.dataset.number * elem.dataset.number;
     }
 }
