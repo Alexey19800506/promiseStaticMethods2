dc599da2a3f657c017a8933274a1abd7e3f42ea3
Alexey19800506 <osiyuk19800506@gmail.com>
2022-07-07 13:12:22 +0300

    t1

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-export function setSection(num) {
-    const elemements = document.querySelectorAll('.box');
-    const elem1 = `span[data-number = "${num}"]`;
-    for (let elem of elemements) {
-        if (elem.dataset == elem1) {
-            return elem.closest('.box').dataset;
-        }
-    }
+const logTarget = (text, color) => {
+    const eventsListElem = document.querySelector('.events-list');
+    eventsListElem.innerHTML += `<span style ="color:${color}; margin-left:8px;">${text}</span>`;
 }
+
+const logGreenDiv = logTarget.bind(null, 'DIV', 'green');
+const logGreenP = logTarget.bind(null, 'P', 'green');
+const logGreenSpan = logTarget.bind(null, 'SPAN', 'green');
+
+const logGreyDiv = logTarget.bind(null, 'DIV', 'grey');
+const logGreyP = logTarget.bind(null, 'P', 'grey');
+const logGreySpan = logTarget.bind(null, 'SPAN', 'grey');
+
+divElem.addEventLIstener('click', logGreyDiv, true);
+divElem.addEventLIstener('click', logGreenDiv);
+
+pElem.addEventLIstener('click', logGreyP, true);
+pElem.addEventLIstener('click', logGreenP);
+
+spanElem.addEventLIstener('click', logGreySpan, true);
+spanElem.addEventLIstener('click', logGreenSpan);
