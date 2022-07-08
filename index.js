05d77f584a3b05811c02fe0fb18fe75af164f6ff
Alexey19800506 <osiyuk19800506@gmail.com>
2022-07-08 11:53:49 +0300

    t3

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const inputElem = document.querySelector('.text-input');
-inputElem.addEventListener('change', () => console.log(inputElem.value));
+const checkboxElem = document.querySelector('.task-status');
+
+function checked() {
+    if (checkboxElem.checked) {
+        console.log(true);
+    } else console.log(false);
+}
+checkboxElem.addEventListener('click', checked);
