e5f6d6c0e76e031efaf86debbf4e4aa427095df0
Alexey19800506 <osiyuk19800506@gmail.com>
2022-07-08 11:59:28 +0300

    t3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const checkboxElem = document.querySelector('.task-status');
 
 function checked() {
     if (checkboxElem.checked) {
-        console.log(true);
-    } else console.log(false);
+        console.log(false);
+    } else console.log(true);
 }
-checkboxElem.addEventListener('click', checked);
+checkboxElem.addEventListener('change', checked);
