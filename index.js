0882da25c0036ac2ecf87633ddc25b33bcbd8523
Alexey19800506 <osiyuk19800506@gmail.com>
2022-07-27 17:36:02 +0300

    t3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ export function getDiff(startDate, endDate) {
     const secondDate = new Date(endDate);
 
     const result = Math.abs(firstDate.getTime() - secondDate.getTime());
-    const days = Math.floor((result / (24 * 60 * 60 * 1000)) % 30);
+    const days = Math.floor(result / (24 * 60 * 60 * 1000));
     const hours = Math.floor((result / (60 * 60 * 1000)) % 24);
     const minuts = Math.floor((result / (60 * 1000)) % 60);
     const seconds = Math.floor((result / 1000) % 60);
