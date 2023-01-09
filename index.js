2054d57c82e4f7270d596552ed7539eac0c3ff49
Alexey19800506 <osiyuk19800506@gmail.com>
2023-01-09 16:36:07 +0200

    pomise

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-export const addImage = (imgSrc, callback) => {
-    const imgElem = document.createElement('img');
-    imgElem.setAttribute('alt', 'My Photo');
-    imgElem.src = imgSrc;
-    const containerElem = document.querySelector('.page');
-    containerElem.append(imgElem);
+export const addImage = (imgSrc) => {
 
-    const onImageLoaded = () => {
-        const { width, height } = imgElem;
-        callback(null, { width, height });
-    }
+    const p = new Promise((resolve, reject) => {
+        const imgElem = document.createElement('img');
+        imgElem.setAttribute('alt', 'My Photo');
+        imgElem.src = imgSrc;
+        const containerElem = document.querySelector('.page');
+        containerElem.append(imgElem);
 
-    imgElem.addEventListener('load', onImageLoaded);
+        const onImageLoaded = () => {
+            const { width, height } = imgElem;
+            resolve({ width, height });
+        }
 
-    imgElem.addEventListener('error', () => callback('Image load is failed'));
+        imgElem.addEventListener('load', onImageLoaded);
+
+        imgElem.addEventListener('error', () => reject(new Error('Image load is failed')));
+    });
+    return p;
 };
 
 const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
 
-const onImageLoaded = (error, data) => {
-    if (error) {
-        console.log(error);
-        return;
-    }
-    const { width, height } = data;
-    const sizeElem = document.querySelector('.image-size');
-    sizeElem.textContent = `${width} x ${height}`;
-}
-
-addImage(imgSrc, onImageLoaded);
+const resultPromise = addImage(imgSrc);
+resultPromise.then(data => console.log(data));
+//resultPromise.catch(error => console.log(error));
