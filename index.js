c9e17f84ec615380c013cd21768eb058c19b21df
Alexey19800506 <osiyuk19800506@gmail.com>
2023-01-10 16:42:27 +0200

    ph

--- index.js
+++ index.js
@@ -1,26 +1,23 @@
-export const addImage = (imgSrc) => {
-
-    const p = new Promise((resolve, reject) => {
-        const imgElem = document.createElement('img');
-        imgElem.setAttribute('alt', 'My Photo');
-        imgElem.src = imgSrc;
-        const containerElem = document.querySelector('.page');
-        containerElem.append(imgElem);
-
-        const onImageLoaded = () => {
-            const { width, height } = imgElem;
-            resolve({ width, height });
+const requestUserData = userId => {
+    const request = new Promise((resolve, reject) => {
+        if (userId === 'broken') {
+            setTimeout(() => {
+                reject(new Error('User not found'));
+            }, 500);
+        } else {
+            setTimeout(() => {
+                resolve({
+                    name: 'John',
+                    age: 17,
+                    email: `${userId}@exsemple.com`,
+                    userId
+                });
+            }, 1000);
         }
-
-        imgElem.addEventListener('load', onImageLoaded);
-
-        imgElem.addEventListener('error', () => reject(new Error('Image load is failed')));
     });
-    return p;
+    return request;
 };
 
-const imgSrc = 'https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg';
-
-const resultPromise = addImage(imgSrc);
-resultPromise.then(data => console.log(data));
-//resultPromise.catch(error => console.log(error));
+requestUserData('broken')
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
